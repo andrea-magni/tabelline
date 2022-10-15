a1e11a3d96adb0db7ad5948aaffe5f68682df065
Andrea Magni <andrea.magni@gmail.com>
2022-10-15 15:23:44 +0200

    Fix Tabelline e Divisioni

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function renderTabelline(inizia_da, finisci_a) {
 		$('#tabelline').append(`			
 			<div class="card tabellina d-print-inline-block">
 				<div class="card-body">
-					${e.res} : ${e.val1} = ....
+					${e.val1} x ${e.val2} = ....
 				</div>
 			</div>
 		`);
@@ -25,13 +25,46 @@ function renderTabelline(inizia_da, finisci_a) {
 }
 
 
+function renderDivisioni(inizia_da, finisci_a) {
+	let tabelline = [];
+
+	for (let x = inizia_da; x<= finisci_a; x++) {
+		for (let y = 0; y<=10; y++) {
+			tabelline.push(
+				{ val1: x, val2: y, res: x*y}
+			);
+		}
+	}
+
+	tabelline = tabelline.sort( () => Math.random() - 0.5);
+
+	$('#tabelline').empty();
+
+	tabelline.map((e) => {
+		$('#tabelline').append(`			
+			<div class="card tabellina d-print-inline-block">
+				<div class="card-body">
+					${e.res} : ${e.val1} = ....
+				</div>
+			</div>
+		`);
+	});
+}
+
 $(document).ready(function(){
 
-	$('#produci').click(
+	$('#produci_tabelline').click(
 		function(){
 			renderTabelline(parseInt($('#inizia_da').val()), parseInt($('#finisci_a').val()))		
 		}
 	);
+
+	$('#produci_divisioni').click(
+		function(){
+			renderDivisioni(parseInt($('#inizia_da').val()), parseInt($('#finisci_a').val()))		
+		}
+	);
+
 
 	$('#stampa').click(
 		function(){
